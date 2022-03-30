4be58f1bbe256b0aa564fcf839c5d45a9f408410
SpawNKZ <kadyrzhanovdias@gmail.com>
2022-03-30 21:34:27 +0600

    twosum

--- 1.two-sum.go
+++ 1.two-sum.go
@@ -1,15 +1,12 @@
 package leetcode
 
 func TwoSum(nums []int, target int) []int {
-	var arr []int
-	for i := 0; i < len(nums); i++ {
+	for i := 0; i < len(nums)-1; i++ {
 		for j := i + 1; j < len(nums); j++ {
 			if nums[i]+nums[j] == target {
-				arr = append(arr, i)
-				arr = append(arr, j)
-				break
+				return []int{i, j}
 			}
 		}
 	}
-	return arr
+	return []int{}
 }
