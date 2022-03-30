e5c12e67aa338d4d664a8911395cd8d79ce3df26
SpawNKZ <kadyrzhanovdias@gmail.com>
2022-03-30 21:26:48 +0600

    changes

--- test/main.go
+++ test/main.go
@@ -6,10 +6,16 @@ import (
 )
 
 func main() {
-	nums := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
-	// num1 := []int{1}
-	// num2 := []int{5, 4, -1, 7, 8}
-	fmt.Println(leetcode.MaxSubArray(nums))
+	// nums1 := []int{1, 2, 3, 0, 0, 0}
+	// m := 3
+	// nums2 := []int{2, 5, 6}
+	// n := 3
+	nums1 := []int{0}
+	m := 0
+	nums2 := []int{1}
+	n := 1
+	leetcode.Merge(nums1, m, nums2, n)
+	fmt.Println(nums1)
 	// fmt.Println(leetcode.MaxSubArray(num1))
 	// fmt.Println(leetcode.MaxSubArray(num2))
 }
