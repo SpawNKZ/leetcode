1c1c8a25884210e6ca18ecbead4d52f7b8e03e14
SpawNKZ <kadyrzhanovdias@gmail.com>
2022-03-30 21:30:23 +0600

    maximumsubarray

--- 53.maximum-subarray.go
+++ 53.maximum-subarray.go
@@ -7,32 +7,19 @@
 // @lc code=start
 package leetcode
 
-import "fmt"
-
 func MaxSubArray(nums []int) int {
-	var arr []int
-	if len(nums) == 1 {
-		return nums[0]
-	}
-	for i := 0; i < len(nums); i++ {
-		sum := i
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i] != nums[j] {
-				sum += nums[j]
-			} else {
-				fmt.Println(sum)
-				arr = append(arr, sum)
-				break
-			}
+	max := nums[0]
+	var sum int
+	for _, v := range nums {
+		sum += v
+		if max < sum {
+			max = sum
+		}
+		if sum < 0 {
+			sum = 0
 		}
 	}
-	Max := arr[0]
-	for i := 0; i < len(arr); i++ {
-		if Max < arr[i] {
-			Max = arr[i]
-		}
-	}
-	return Max
+	return max
 }
 
 // @lc code=end
