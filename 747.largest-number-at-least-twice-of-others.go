846e045db461d7ddb307ec15ffc9bcfec079761c
SpawNKZ <kadyrzhanovdias@gmail.com>
2022-04-13 18:51:19 +0600

    Largest Number At Least Twice of Others

--- 747.largest-number-at-least-twice-of-others.go
+++ 747.largest-number-at-least-twice-of-others.go
@@ -8,17 +8,11 @@ func DominantIndex(nums []int) int {
 	var index int
 	for i, v := range nums {
 		if max < v {
-			max = v
-			index = i
+			max, index = v, i
 		}
 	}
 	for _, v := range nums {
-		if v == max {
-			continue
-		}
-		var res int
-		res += v * 2
-		if max < res {
+		if v != max && v*2 > max {
 			return -1
 		}
 	}
